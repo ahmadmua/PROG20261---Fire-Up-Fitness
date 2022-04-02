2358373c6e092663141ededa00ec416c85da9891
Muaz Ahmad <ahmadmua@sheridancollege.ca>
2022-04-02 16:12:15 -0400

    Form validation and Exception handling

--- lib/login-screen.dart
+++ lib/login-screen.dart
@@ -1,13 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:project/register-screen.dart';
 
 import 'home-screen.dart';
-import 'welcome-screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
-
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 
@@ -17,6 +14,8 @@ class _LoginScreenState extends State<LoginScreen> {
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  var errorMsg = "";
 
   //User? user = FirebaseAuth.instance.currentUser;
 
@@ -28,12 +27,14 @@ class _LoginScreenState extends State<LoginScreen> {
     Widget? prefixedIcon,
     String? hintText,
     required var typeTxt,
+    FormFieldValidator<String>? validator
   }) {
     return Material(
       color: Colors.transparent,
       elevation: 2,
-      child: TextField(
+      child: TextFormField(
         controller: typeTxt,
+        validator: validator,
         cursorColor: Colors.white,
         cursorWidth: 2,
         obscureText: obscureText,
@@ -44,6 +45,12 @@ class _LoginScreenState extends State<LoginScreen> {
           fillColor: const Color.fromRGBO(255, 159, 129, 1.0),
           prefixIcon: prefixedIcon,
           hintText: hintText,
+          errorStyle: const TextStyle(
+            fontSize: 15.0,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.red,
+            color: Colors.black
+          ),
           hintStyle: const TextStyle(
             color: Colors.white54,
             fontWeight: FontWeight.bold,
@@ -82,15 +89,22 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
         ),
         onPressed: () async{
-          await FirebaseAuth.instance.signInWithEmailAndPassword(
-              email: emailController.text, password: passwordController.text);
 
-           Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return const HomeScreen();
-          }));
+          try {
+            if (_key.currentState!.validate()) {
+              await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: emailController.text,
+                  password: passwordController.text);
 
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const HomeScreen();
+              }));
+            }
+            errorMsg = "";
+          }on FirebaseAuthException catch(error){
+            errorMsg = error.message!;
+          }
           setState(() {});
-
         },
       ),
     );
@@ -99,108 +113,142 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromRGBO(255, 130, 100, 1),
-                Color.fromRGBO(255, 136, 106, 1.0),
-                Color.fromRGBO(255, 138, 108, 1.0),
-                Color.fromRGBO(255, 146, 123, 1.0),
-                Color.fromRGBO(255, 153, 128, 1.0),
-              ],
-            ),
-          ),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 40,
-              ).copyWith(top: 110),
-              child: Column(
-                children: [
-                  const Text(
-                    'Sign In',
-                    style: TextStyle(
-                      fontFamily: 'PT-Sans',
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: const Text(
-                      'Email',
-                      style: TextStyle(
-                        fontFamily: 'PT-Sans',
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  _buildTextField(
-                    hintText: 'Enter your email',
-                    obscureText: false,
-                    prefixedIcon: const Icon(Icons.mail, color: Colors.white),
-                    typeTxt: emailController
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: const Text(
-                      'Password',
-                      style: TextStyle(
-                        fontFamily: 'PT-Sans',
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  _buildTextField(
-                    hintText: 'Enter your password',
-                    obscureText: true,
-                    prefixedIcon: const Icon(Icons.lock, color: Colors.white),
-                    typeTxt: passwordController
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  _buildLoginButton(),
-                  const SizedBox(
-                    height: 20,
-                  ),
-
+        body: Form(
+          key: _key,
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromRGBO(255, 130, 100, 1),
+                  Color.fromRGBO(255, 136, 106, 1.0),
+                  Color.fromRGBO(255, 138, 108, 1.0),
+                  Color.fromRGBO(255, 146, 123, 1.0),
+                  Color.fromRGBO(255, 153, 128, 1.0),
                 ],
+              ),
+            ),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                ).copyWith(top: 110),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontFamily: 'PT-Sans',
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        'Email',
+                        style: TextStyle(
+                          fontFamily: 'PT-Sans',
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    _buildTextField(
+                      hintText: 'Enter your email',
+                      obscureText: false,
+                      prefixedIcon: const Icon(Icons.mail, color: Colors.white),
+                      typeTxt: emailController,
+                        validator: validateEmail
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        'Password',
+                        style: TextStyle(
+                          fontFamily: 'PT-Sans',
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    _buildTextField(
+                      hintText: 'Enter your password',
+                      obscureText: true,
+                      prefixedIcon: const Icon(Icons.lock, color: Colors.white),
+                      typeTxt: passwordController,
+                      validator: validatePassword
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    _buildLoginButton(),
+                    const SizedBox(
+                      height: 60,
+                    ),
+                    Center(
+                      child: Text(
+                          errorMsg,
+                        style: const TextStyle(
+                            color: Color.fromRGBO(246, 6, 6, 1.0),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
-
     );
   }
+}
+
+String? validateEmail(String? formEmail){
+  if(formEmail == null || formEmail.isEmpty){
+    return 'E-mail address is required.';
+  }
+
+  String pattern = r'\w+@\w+\.\w+';
+  RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(formEmail)) return 'Invalid E-mail Address format.';
+
+  return null;
+}
+
+String? validatePassword(String? formPass){
+  if(formPass == null || formPass.isEmpty){
+    return 'Password is required.';
+  }
+
+
+  return null;
 
 }
+
--- lib/register-screen.dart
+++ lib/register-screen.dart
@@ -13,6 +13,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  var errorMsg = "";
 
   bool? isChecked = false;
 
@@ -21,17 +23,25 @@ class _RegisterScreenState extends State<RegisterScreen> {
     Widget? prefixedIcon,
     String? hintText,
     required var typeTxt,
+    FormFieldValidator<String>? validator
   }) {
     return Material(
       color: Colors.transparent,
       elevation: 2,
-      child: TextField(
+      child: TextFormField(
         controller: typeTxt,
         cursorColor: Colors.white,
         cursorWidth: 2,
         obscureText: obscureText,
+        validator: validator,
         style: const TextStyle(color: Colors.white),
         decoration: InputDecoration(
+          errorStyle: const TextStyle(
+              fontSize: 15.0,
+              fontWeight: FontWeight.bold,
+              backgroundColor: Colors.red,
+              color: Colors.black
+          ),
           border: InputBorder.none,
           filled: true,
           fillColor: const Color.fromRGBO(255, 159, 129, 1.0),
@@ -75,12 +85,19 @@ class _RegisterScreenState extends State<RegisterScreen> {
           ),
         ),
         onPressed: () async {
-          await FirebaseAuth.instance.createUserWithEmailAndPassword(
-              email: emailController.text, password: passwordController.text);
-
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return const LoginScreen();
-          }));
+          try {
+            if (_key.currentState!.validate()) {
+              await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: emailController.text,
+                  password: passwordController.text);
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const LoginScreen();
+              }));
+            }
+            errorMsg = "";
+          }on FirebaseAuthException catch(error){
+            errorMsg = error.message!;
+          }
 
           setState(() {});
         },
@@ -95,105 +112,120 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromRGBO(255, 130, 100, 1),
-                Color.fromRGBO(255, 136, 106, 1.0),
-                Color.fromRGBO(255, 138, 108, 1.0),
-                Color.fromRGBO(255, 146, 123, 1.0),
-                Color.fromRGBO(255, 153, 128, 1.0),
-              ],
-            ),
-          ),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 40,
-              ).copyWith(top: 110),
-              child: Column(
-                children: [
-                  const Text(
-                    'Register',
-                    style: TextStyle(
-                      fontFamily: 'PT-Sans',
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: const Text(
-                      'Email',
-                      style: TextStyle(
-                        fontFamily: 'PT-Sans',
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  _buildTextField(
-                    hintText: 'Enter your email',
-                    obscureText: false,
-                    prefixedIcon: const Icon(Icons.mail, color: Colors.white),
-                    typeTxt: emailController
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: const Text(
-                      'Password',
-                      style: TextStyle(
-                        fontFamily: 'PT-Sans',
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  _buildTextField(
-                    hintText: 'Enter your password',
-                    obscureText: true,
-                    prefixedIcon: const Icon(Icons.lock, color: Colors.white),
-                    typeTxt: passwordController
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  _buildRegisterButton(),
-                  const SizedBox(
-                    height: 20,
-                  ),
-
-                  const SizedBox(
-                    height: 30,
-                    width: 50,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
+        body: Form(
+          key: _key,
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromRGBO(255, 130, 100, 1),
+                  Color.fromRGBO(255, 136, 106, 1.0),
+                  Color.fromRGBO(255, 138, 108, 1.0),
+                  Color.fromRGBO(255, 146, 123, 1.0),
+                  Color.fromRGBO(255, 153, 128, 1.0),
                 ],
+              ),
+            ),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                ).copyWith(top: 110),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Register',
+                      style: TextStyle(
+                        fontFamily: 'PT-Sans',
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        'Email',
+                        style: TextStyle(
+                          fontFamily: 'PT-Sans',
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    _buildTextField(
+                      hintText: 'Enter your email',
+                      obscureText: false,
+                      prefixedIcon: const Icon(Icons.mail, color: Colors.white),
+                      typeTxt: emailController,
+                      validator: validateEmail
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        'Password',
+                        style: TextStyle(
+                          fontFamily: 'PT-Sans',
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    _buildTextField(
+                      hintText: 'Enter your password',
+                      obscureText: true,
+                      prefixedIcon: const Icon(Icons.lock, color: Colors.white),
+                      typeTxt: passwordController,
+                      validator: validatePassword
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    _buildRegisterButton(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    const SizedBox(
+                      height: 30,
+                      width: 50,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Center(
+                      child: Text(
+                        errorMsg,
+                        style: const TextStyle(
+                            color: Color.fromRGBO(246, 6, 6, 1.0),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -201,4 +233,26 @@ class _RegisterScreenState extends State<RegisterScreen> {
       ),
     );
   }
+}
+
+String? validateEmail(String? formEmail){
+  if(formEmail == null || formEmail.isEmpty){
+    return 'E-mail address is required.';
+  }
+
+  String pattern = r'\w+@\w+\.\w+';
+  RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(formEmail)) return 'Invalid E-mail Address format.';
+
+  return null;
+}
+
+String? validatePassword(String? formPass){
+  if(formPass == null || formPass.isEmpty){
+    return 'Password is required.';
+  }
+
+
+  return null;
+
 }
