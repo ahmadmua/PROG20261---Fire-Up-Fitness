a38a5241ed8fec3fb4f23c0fc2b1917dbd1103ed
Nicholas Cammisuli <cammisun@sheridancollege.ca>
2022-04-09 17:34:11 -0400

    calendar event functionality

--- lib/calendar-screen.dart
+++ lib/calendar-screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:project/calendarEvent/event.dart';
 import 'package:project/home-screen.dart';
 import 'package:project/pedometer-screen.dart';
 import 'package:project/workEd-screen.dart';
@@ -16,9 +17,29 @@ class CalendarScreen extends StatefulWidget {
 
 class _CalendarScreenState extends State<CalendarScreen> {
   int _currentIndex = 1;
-  DateTime? _selectedDay;
+
+  Map<DateTime, List<Event>> _selectedEvents = {};
+  DateTime _selectedDay = DateTime.now();
   DateTime _focusedDay = DateTime.now();
   CalendarFormat _calendarFormat = CalendarFormat.month;
+
+  final TextEditingController _eventController = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+    _selectedEvents = {};
+  }
+
+  List<Event> getEventsFromDay(DateTime date){
+    return _selectedEvents[date]?? [];
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    _eventController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -88,29 +109,91 @@ class _CalendarScreenState extends State<CalendarScreen> {
             ),
           ],
         ),
-        body: TableCalendar(
-          firstDay: DateTime.utc(2015, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: DateTime.now(),
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay; // update `_focusedDay` here as well
-            });
-          },
-          calendarFormat: _calendarFormat,
-          onFormatChanged: (format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          },
-          onPageChanged: (focusedDay) {
-            _focusedDay = focusedDay;
-          },
+        body: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2015, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              },
+              eventLoader: getEventsFromDay,
+              calendarFormat: _calendarFormat,
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+              headerStyle: const HeaderStyle(
+                formatButtonShowsNext: false
+              ),
+              calendarStyle: const CalendarStyle(
+                selectedDecoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 130, 100, 1),
+                  shape: BoxShape.circle,
+                ),
+                todayDecoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 130, 100, 0.7),
+                  shape: BoxShape.circle,
+                )
+              ),
+            ),
+            ...getEventsFromDay(_selectedDay).map((Event event) => ListTile(title: Text(event.title),)),
+          ],
         ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text("Add Event"),
+            content: TextFormField(
+              controller: _eventController,
+            ),
+            actions: [
+              TextButton(
+                child: const Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+              TextButton(
+                child: const Text("Ok"),
+                onPressed: () {
+                  if (_eventController.text.isEmpty) {
+
+                  } else {
+                    if (_selectedEvents[_selectedDay] != null) {
+                      _selectedEvents[_selectedDay]?.add(
+                        Event(title: _eventController.text),
+                      );
+                    } else {
+                      _selectedEvents[_selectedDay] = [
+                        Event(title: _eventController.text)
+                      ];
+                    }
+
+                  }
+                  Navigator.pop(context);
+                  _eventController.clear();
+                  setState((){});
+                  return;
+                },
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: const Color.fromRGBO(255, 130, 100, 1),
+        label: const Text("Add Event"),
+        icon: const Icon(Icons.add),
+      ),
     );
   }
 }
