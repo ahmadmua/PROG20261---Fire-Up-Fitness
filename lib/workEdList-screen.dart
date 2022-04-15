f1a201750e6fdcee98b6f6ecf27f0f2124838fbc
Jordan Bhar <bharjor@sheridancollege.ca>
2022-04-14 20:30:17 -0400

    Implemented Dynamic Navigation in workEdList-screen.dart, also got rid of an uneeded elevated buttion, this also fixed blue background

--- lib/workEdList-screen.dart
+++ lib/workEdList-screen.dart
@@ -128,25 +128,23 @@ class _WorkEdListState extends State<WorkEdList>{
         ],
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {  },
-          child: Flexible( child: FirebaseAnimatedList(query: _workoutRef, itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index) {
+        child: Flexible( child: FirebaseAnimatedList(query: _workoutRef, itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index) {
               Map workouts = snapshot.value as Map;
 
               return ListTile(title:  Text(snapshot.key.toString()),
 
                 onTap:() {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => WorkEdDetailScreen(data: data,)));
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => WorkEdDetailScreen(data: data[snapshot.key.toString()],)
 
-                      //  WorkEdDetailScreen(data: data["Pullup"] : this line of code needs to be changed were "pullup" is to a dynamic way of navigating to the detail page with the exercise
+                      ));
                   },
                 );
             },
             )
           )
         )
-      )
-    );
+      );
   }
 }
 
