ffbe9e76c49978c31115def6505dc2a7c964d6b0
Jordan Bhar <bharjor@sheridancollege.ca>
2022-04-15 00:25:27 -0400

    added new dependancy, also implemented youtube player to use the url link grabed from database to display coresponding video

--- lib/workEdDetail-screen.dart
+++ lib/workEdDetail-screen.dart
@@ -6,6 +6,7 @@ import 'package:project/pedometer-screen.dart';
 import 'package:project/settings.dart';
 import 'package:project/workEd-screen.dart';
 import 'package:project/workEdList-screen.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 import 'calendar-screen.dart';
 import 'createWorkout-screen.dart';
@@ -19,19 +20,35 @@ class WorkEdDetailScreen extends StatefulWidget {
   State<WorkEdDetailScreen> createState() => _WorkEdDetailScreenState();
 }
 
-class _WorkEdDetailScreenState extends State<WorkEdDetailScreen> with SingleTickerProviderStateMixin {
+class _WorkEdDetailScreenState extends State<WorkEdDetailScreen>
+    with SingleTickerProviderStateMixin {
+  late YoutubePlayerController videoController;
 
+  void loadVideo() {
+    var url = (widget.data['url'].toString());
+    videoController = YoutubePlayerController(
+        initialVideoId: (YoutubePlayer.convertUrlToId(url) as String),
+        flags: YoutubePlayerFlags(enableCaption: false, autoPlay: true));
+  }
+
+  @override
+  void initState() {
+    loadVideo();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(icon: const Icon(Icons.arrow_back), onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context){
-              return WorkEdList(index: widget.data['parent'] );
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return WorkEdList(index: widget.data['parent']);
             }));
           },
-          ),
+        ),
         automaticallyImplyLeading: false,
         title: Text(widget.data['name'].toString()),
         actions: [
@@ -46,12 +63,36 @@ class _WorkEdDetailScreenState extends State<WorkEdDetailScreen> with SingleTick
         ],
         backgroundColor: const Color.fromRGBO(255, 130, 100, 1),
       ),
-    body: Center(
+      body: Center(
+        //TODO display all feilds being passed over within the data variable passed from the other page
+        //TODO Milestone: Implement and embeded youtube player in the app to show video content from youtube
 
-      //TODO display all feilds being passed over within the data variable passed from the other page
-      //TODO Milestone: Implement and embeded youtube player in the app to show video content from youtube
-
-      child: Text(widget.data['name'].toString()),) ,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 40.0),
+              padding:EdgeInsets.all(25.0) ,
+              child: YoutubePlayerBuilder(
+                player: YoutubePlayer(
+                  controller: videoController,
+                ),
+                builder: (context, player) {
+                  return Column(
+                    children: <Widget>[
+                      player,
+                      const SizedBox(
+                        height: 40.0,
+                      )
+                    ],
+                  );
+                },
+              ),
+            ),
+            Text(widget.data['targetGroups'].toString()),
+            Text(widget.data['description'].toString())
+          ],
+        ),
+      ),
     );
   }
 }
